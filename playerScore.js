4cab05b5650c212b64fe8eaa2f83653b27f821af
Benedict Ojekudo <59806784+benefied@users.noreply.github.com>
2022-12-16 23:06:33 +0100

    Create playerScore.js
    the main commit

---
+++ playerScore.js
@@ -0,0 +1,33 @@
+function playerHandScore(hand) {
+    const scores = {
+        "K": 4,
+        "Q": 3,
+        "J": 2
+    }
+
+    const handArr = hand.split("");
+    let sum = 0;
+    for(let i = 0; i < handArr.length; i++) {
+        const score = scores[handArr[i]];
+        sum += score;
+    }
+
+    return sum;
+}
+
+
+
+/*
+  The function playerHandScore that takes in a string of face cards (Jack, Queen, and King only) and returns the total score of the players hand.
+
+The cards are represented by the first letter in the name of the card:
+
+A "K" is 4 points
+A "Q" is 3 points
+A "J" is 2 points
+Example Usage:
+
+console.log( playerHandScore("K") ); // 4
+console.log( playerHandScore("KJ") ); // 6
+console.log( playerHandScore("KQQ") ); // 10 
+*/
